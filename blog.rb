0571381cfdc19fec7d077cb33770ae2396c21513
Paulo Souza <psss04@outlook.com>
2015-11-28 22:50:19 -0200

    Criação do arquivo do blog

---
+++ blog.rb
@@ -0,0 +1,22 @@
+def visitors_menu
+end
+def users_menu
+end
+def create_user
+end
+def view_user
+end
+def edit_user
+end
+def delete_user
+end
+def create_post
+end
+def view_post
+end
+def edit_post
+end
+def delete_post
+end
+def show_all_posts
+end
